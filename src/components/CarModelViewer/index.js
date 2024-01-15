21af91c1954b2baf9e641e74c23335aaf046eeb3
JaimeFerreira2002 <jaimeslopesferreira@gmail.com>
2024-01-15 03:46:50 +0000

    Worked on the FSFenix Page, added 3d Models and specs, added new fonts to project, made changes to the topbar

---
+++ src/components/CarModelViewer/index.js
@@ -0,0 +1,41 @@
+import React, { useRef, Suspense } from 'react';
+import { Canvas, extend, useFrame, useThree } from 'react-three-fiber';
+import FSFenixModel from '../../assets/3DModels/FSFenix_Render.js';
+import { OrbitControls } from '@react-three/drei';
+
+
+// Main component to display the 3D model
+const CarModelViewer = () => { 
+
+  return (
+    //this is three.js, every code inside the Canvas is Three.js code
+    //canvas is used to render 3d stuff
+    <Canvas camera={{
+      position: [0, 4, 10],  
+      fov: 35,              
+    }}>
+      <Suspense fallback={null}> 
+        <ambientLight/>
+        <directionalLight intensity={2} angle={0.1} penumbra={1} position={[10,15,10]} castShadow/>
+        <FSFenixModel/>
+        <OrbitControls autoRotate={true} 
+                       autoRotateSpeed={0.5} 
+                       enablePan={false} 
+                       enableZoom = {false} 
+                       enableRotate = {true} 
+                       maxZoom={1}
+                       minPolarAngle={Math.PI / 6} 
+                       maxPolarAngle={Math.PI / 2} 
+                       dampingFactor={0.2}
+                       />
+
+      </Suspense>
+    </Canvas>
+  );
+};
+
+
+export default CarModelViewer;
+
+
+  
--- src/pages/FSFenixPage/index.js
+++ src/pages/FSFenixPage/index.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useRef, useState } from 'react';
 import "./style.css";
 import { useTranslation } from "react-i18next";
+import CarModelViewer from '../../components/CarModelViewer';
+import SponsorsCard from '../../assets/images/FSFenix_Sponsors.png'
 
 
 const FSFenixPage = () => {
   const { t } = useTranslation();
+  const ref = useRef();
   return (
-    <div>
-      <h2>FSFenix Page</h2>
-      <p>This is the FSFENIX page. Content goes here.</p>
+    <div className='fsfsenix-page'>
+        <div className='page-header'>
+          <h2 className='page-title'>{t('fsfenix-page.page-title')}</h2>
+          <p className='page-subtitle'>{t('fsfenix-page.page-subtitle')}</p>
+        </div>
+
+        <div className='model_and_specs'>
+            <div className='specs'>
+            <h2 className='specs-title'>Technical Specifications</h2>
+              <ul>
+                <li>Engine Type - V6</li>
+                <li>Horsepower - 300 HP</li>
+                <li>Transmission - Automatic</li>
+                <li>Fuel Efficiency - 25 MPG</li>
+                <li>Color Options - Red, Blue, Silver</li>
+                <li>Safety Features - ABS, Airbags</li>
+                <li>Warranty - 3 years</li>
+              </ul>
+            </div>
+            <div className="fsfenix-3d-model">        
+                <CarModelViewer/>         
+            </div>
+          </div>
+
+          {/* <div className='sponsors-card'>
+            <img src={SponsorsCard} alt={"Sponsor-Photo"} className="sponsors-card-photo" />
+          </div> */}
     </div>
   );
 };
