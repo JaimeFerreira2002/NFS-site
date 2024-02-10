9540d4f48d9dbb8081f298bf512ddfba7d040295
Miguel Santos <57097067+mccSantos@users.noreply.github.com>
2024-02-10 03:01:55 +0000

    About us page done at least the functional part
    Extra design might come xD

--- src/pages/AboutUsPage/AboutUsPage.js
+++ src/pages/AboutUsPage/AboutUsPage.js
@@ -1,80 +1,34 @@
-import React, { useState } from "react";
 import "./style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faPeopleGroup,
   faTag,
   faPenRuler,
   faChartLine,
-  faGaugeHigh, 
+  faGaugeHigh,
   faCarSide,
   faBatteryHalf,
-  faFlagCheckered,
   faRoad,
   faCar,
-  faGears,
 } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "react-i18next";
-import { OrganizationChart } from "primereact/organizationchart";
+import Accordion from "react-bootstrap/Accordion";
+import eng_team_chart from "../../assets/images/org_chart_eng.drawio.png";
+import pt_team_chart from "../../assets/images/org_chart_pt.drawio.png";
 
 const AboutUsPage = () => {
-  const data = [
-    {
-      label: "Team Leader",
-      expanded: true,
-      children: [
-        {
-          label: "Techinical Director",
-          expanded: true,
-          children: [
-            {
-              label: "Electronics",
-            },
-            {
-              label: "Steering & Ergonomics",
-            },
-            {
-              label: "Chassis",
-            },
-            {
-              label: "Powertrain",
-            },
-            {
-              label: "Aerodynamics",
-            },
-            {
-              label: "Vehicle Dynamics",
-            },
-          ],
-        },
-        {
-          label: "Business & Strategy Director",
-          expanded: true,
-          children: [
-            {
-              label: "Marketing",
-            },
-            {
-              label: "Logistics",
-            },
-            {
-              label: "Finance",
-            },
-            {
-              label: "Corporate Relations",
-            },
-          ],
-        },
-      ],
-    },
-  ];
   const { t } = useTranslation();
   return (
     <div className="page-about">
       <div className="section">
-        <span style={{display:"flow"}}>
-        <h1 style={{ textAlign: "center", fontSize:"3.5rem" }}>{t("about-page.about-us")}</h1>
-        <h3 style={{marginLeft:"12.5%", textAlign: "center", width:"75%"}}>{t("about-page.about-us-desc")}</h3>
+        <span style={{ display: "flow" }}>
+          <h1 style={{ textAlign: "center", fontSize: "3.5rem" }}>
+            {t("about-page.about-us")}
+          </h1>
+          <h3
+            style={{ marginLeft: "12.5%", textAlign: "center", width: "75%" }}
+          >
+            {t("about-page.about-us-desc")}
+          </h3>
         </span>
         <br></br>
         <br></br>
@@ -83,11 +37,12 @@ const AboutUsPage = () => {
         <h2>
           <strong>{t("home.team")}</strong>
         </h2>
+        <img
+          src={t("topbar.start") === "Home" ? eng_team_chart : pt_team_chart}
+          alt="Team Organization"
+        />
         <br></br>
         <br></br>
-        <div>
-          <OrganizationChart value={data} style={{ margin: "auto" }} />
-        </div>
         <h2>
           <strong>{t("about-page.how-it-works-title")}</strong>
         </h2>
@@ -101,62 +56,65 @@ const AboutUsPage = () => {
         <br></br>
         <p>{t("about-page.static-ev-desc")}</p>
         <br></br>
-        <p style={{ display: "flex" }}>
-          <FontAwesomeIcon
-            icon={faPenRuler}
-            size="4x"
-            color="#419ed4"
-            style={{ paddingRight: "2rem" }}
-          />
-          <span>
-            <span style={{ display: "flex" }}>
-              <h4>{t("about-page.business-model")}</h4>
-              <small style={{ padding: "0.5rem" }}>
-                75 {t("about-page.points")}
-              </small>
-            </span>
-            <span>{t("about-page.business-model-desc")}</span>
-          </span>
-        </p>
-        <br></br>
-        <br></br>
-        <p style={{ display: "flex" }}>
-          <FontAwesomeIcon
-            icon={faTag}
-            size="4x"
-            color="#419ed4"
-            style={{ paddingRight: "2rem" }}
-          />
-          <span>
-            <span style={{ display: "flex" }}>
-              <h4>{t("about-page.cost-manufacturing")}</h4>
-              <small style={{ padding: "0.5rem" }}>
-                100 {t("about-page.points")}
-              </small>
-            </span>
-              <span>{t("about-page.cost-manufacturing-desc")}</span>
-          </span>
-        </p>
-        <br></br>
-        <br></br>
-        <p style={{ display: "flex" }}>
-          <FontAwesomeIcon
-            icon={faChartLine}
-            size="4x"
-            color="#419ed4"
-            style={{ paddingRight: " 2rem" }}
-          />
-          <span>
-            <span style={{ display: "flex" }}>
-              <h4>{t("about-page.engineering-design")}</h4>
-              <small style={{ padding: "0.5rem" }}>
-                150 {t("about-page.points")}
-              </small>
-            </span>
-              <span>{t("about-page.engineering-desc")}</span>
-          </span>
-        </p>
-        <br></br>
+        <Accordion defaultActiveKey="-1">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>
+              <FontAwesomeIcon
+                icon={faPenRuler}
+                size="4x"
+                color="#419ed4"
+                style={{ paddingRight: "2rem" }}
+              />
+
+              <span style={{ display: "flex" }}>
+                <h4>{t("about-page.business-model")}</h4>
+                <small style={{ padding: "0.5rem" }}>
+                  75 {t("about-page.points")}
+                </small>
+              </span>
+            </Accordion.Header>
+            <Accordion.Body>
+              {t("about-page.business-model-desc")}
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>
+              <FontAwesomeIcon
+                icon={faTag}
+                size="4x"
+                color="#419ed4"
+                style={{ paddingRight: "2rem" }}
+              />
+              <span style={{ display: "flex" }}>
+                <h4>{t("about-page.cost-manufacturing")}</h4>
+                <small style={{ padding: "0.5rem" }}>
+                  100 {t("about-page.points")}
+                </small>
+              </span>
+            </Accordion.Header>
+            <Accordion.Body>
+              {t("about-page.cost-manufacturing-desc")}
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>
+              <FontAwesomeIcon
+                icon={faChartLine}
+                size="4x"
+                color="#419ed4"
+                style={{ paddingRight: " 2rem" }}
+              />
+              <span style={{ display: "flex" }}>
+                <h4>{t("about-page.engineering-design")}</h4>
+                <small style={{ padding: "0.5rem" }}>
+                  150 {t("about-page.points")}
+                </small>
+              </span>
+            </Accordion.Header>
+            <Accordion.Body>{t("about-page.engineering-desc")}</Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+
         <br></br>
         <br></br>
         <h3 style={{ textAlign: "center" }}>
@@ -166,97 +124,94 @@ const AboutUsPage = () => {
         <p>{t("about-page.dynamic-ev-desc")}</p>
         <br></br>
 
-        <p style={{ display: "flex" }}>
-          <FontAwesomeIcon
-            icon={faGaugeHigh}
-            size="4x"
-            color="#419ed4"
-            style={{ paddingRight: "2rem" }}
-          />
-          <span>
-            <span style={{ display: "flex" }}>
-              <h4>{t("about-page.acceleration")}</h4>
-              <small style={{ padding: "0.5rem" }}>
-                75 {t("about-page.points")}
-              </small>
-            </span>
+        <Accordion defaultActiveKey="-1">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>
+              <FontAwesomeIcon
+                icon={faGaugeHigh}
+                size="4x"
+                color="#419ed4"
+                style={{ paddingRight: "2rem" }}
+              />
+              <span style={{ display: "flex" }}>
+                <h4>{t("about-page.acceleration")}</h4>
+                <small style={{ padding: "0.5rem" }}>
+                  75 {t("about-page.points")}
+                </small>
+              </span>
+            </Accordion.Header>
+            <Accordion.Body>{t("about-page.acceleration-desc")}</Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>
+              <FontAwesomeIcon
+                icon={faCarSide}
+                size="4x"
+                color="#419ed4"
+                style={{ paddingRight: "1.4rem" }}
+              />
+              <span style={{ display: "flex" }}>
+                <h4>{t("about-page.skidpad")}</h4>
+                <small style={{ padding: "0.5rem" }}>
+                  75 {t("about-page.points")}
+                </small>
+              </span>
+            </Accordion.Header>
+            <Accordion.Body>{t("about-page.skidpad-desc")}</Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>
+              <FontAwesomeIcon
+                icon={faCar}
+                size="4x"
+                color="#419ed4"
+                style={{ paddingRight: "2rem" }}
+              />
+              <span style={{ display: "flex" }}>
+                <h4>{t("about-page.autocross")}</h4>
+                <small style={{ padding: "0.5rem" }}>
+                  100 {t("about-page.points")}
+                </small>
+              </span>
+            </Accordion.Header>
+            <Accordion.Body>{t("about-page.autocross-desc")}</Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>
+              <FontAwesomeIcon
+                icon={faRoad}
+                size="4x"
+                color="#419ed4"
+                style={{ paddingRight: "1.7rem" }}
+              />
 
-            <span>{t("about-page.acceleration-desc")}</span>
-          </span>
-        </p>
-        <br></br>
-        <p style={{ display: "flex" }}>
-          <FontAwesomeIcon
-            icon={faCarSide}
-            size="4x"
-            color="#419ed4"
-            style={{ paddingRight: "1.4rem" }}
-          />
-          <span>
-            <span style={{ display: "flex" }}>
-              <h4>{t("about-page.skidpad")}</h4>
-              <small style={{ padding: "0.5rem" }}>
-                75 {t("about-page.points")}
-              </small>
-            </span>
-            <span>{t("about-page.skidpad-desc")}</span>
-          </span>
-        </p>
-        <br></br>
-        <p style={{ display: "flex" }}>
-          <FontAwesomeIcon
-            icon={faCar}
-            size="4x"
-            color="#419ed4"
-            style={{ paddingRight: "2rem" }}
-          />
-          <span>
-            <span style={{ display: "flex" }}>
-              <h4>{t("about-page.autocross")}</h4>
-              <small style={{ padding: "0.5rem" }}>
-                100 {t("about-page.points")}
-              </small>
-            </span>
-            <span>{t("about-page.autocross-desc")}</span>
-          </span>
-        </p>
-        <br></br>
-        <p style={{ display: "flex" }}>
-          <FontAwesomeIcon
-            icon={faRoad}
-            size="4x"
-            color="#419ed4"
-            style={{ paddingRight: "1.7rem" }}
-          />
-          <span>
-            <span style={{ display: "flex" }}>
-              <h4>{t("about-page.efficiency")}</h4>
-              <small style={{ padding: "0.5rem" }}>
-                100 {t("about-page.points")}
-              </small>
-            </span>
-            <span>{t("about-page.efficiency-desc")}</span>
-          </span>
-        </p>
-        <br></br>
-        <p style={{ display: "flex" }}>
-          <FontAwesomeIcon
-            icon={faBatteryHalf}
-            size="4x"
-            color="#419ed4"
-            style={{ paddingRight: "1.8rem" }}
-          />
-          <span>
-            <span style={{ display: "flex" }}>
-              <h4>{t("about-page.endurance")}</h4>
-              <small style={{ padding: "0.5rem" }}>
-                300 {t("about-page.points")}
-              </small>
-            </span>
-            <span>{t("about-page.endurance-desc")}</span>
-          </span>
-        </p>
-        <br></br>
+              <span style={{ display: "flex" }}>
+                <h4>{t("about-page.efficiency")}</h4>
+                <small style={{ padding: "0.5rem" }}>
+                  100 {t("about-page.points")}
+                </small>
+              </span>
+            </Accordion.Header>
+            <Accordion.Body>{t("about-page.efficiency-desc")}</Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="4">
+            <Accordion.Header>
+              <FontAwesomeIcon
+                icon={faBatteryHalf}
+                size="4x"
+                color="#419ed4"
+                style={{ paddingRight: "1.8rem" }}
+              />
+              <span style={{ display: "flex" }}>
+                <h4>{t("about-page.endurance")}</h4>
+                <small style={{ padding: "0.5rem" }}>
+                  300 {t("about-page.points")}
+                </small>
+              </span>
+            </Accordion.Header>
+            <Accordion.Body>{t("about-page.endurance-desc")}</Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
       </div>
     </div>
   );
