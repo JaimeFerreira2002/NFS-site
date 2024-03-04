1ba26245ca44938d761cfa6633b9f905b633c691
JHGarcia <jh.garcia@campus.fct.unl.pt>
2024-03-04 15:56:30 +0000

    contact page updated

--- src/pages/ContactsPage/index.js
+++ src/pages/ContactsPage/index.js
@@ -57,61 +57,61 @@ const ContactsPage = () => {
   return (
     <div>
       <div className='contacts-page-container'>
-
         <div className='contacts-content'>
-
-          <div className='contact-form-section'>
-            <h2 className="form-title">{t('contacts-page.form-title')}</h2>
-            {feedbackMessage && (<div style={{ color: feedbackMessageType === 'success' ? 'green' : 'red' }}>
-              {feedbackMessage}
-            </div>)}
-            <form className='contact-form' ref={form} onSubmit={sendEmail}>
-
-              <input type='text' placeholder={t('contacts-page.form-subject')} />
-
-              <input type='email' onChange={(e) => setSubject(e.target.value)} placeholder={t('contacts-page.form-email')} />
-
-              <textarea onChange={(e) => setEmail(e.target.value)} placeholder={t('contacts-page.form-message')}></textarea>
-              <button type='submit'>{t('contacts-page.form-send')} </button>
-            </form>
-          </div>
-
           <div className='contact-info-section'>
             <h2 className="contacts-page-title info-title">{t('contacts-page.page-title')}</h2>
             <div className="contact-info-columns">
-              <div>
+              <div className='contact-sub-section '>
                 <h3>{t('contacts-page.team-email')}</h3>
                 <p>{"formulastudent@ae.fct.unl.pt"}</p>
               </div>
-              <div>
+              <div className='contact-sub-section '>
                 <h3>{t('contacts-page.cr-email')}</h3>
                 <p>{"cr.formualstudent@ae.fct.unl.pt"}</p>
               </div>
-              <div>
+              <div className='contact-sub-section '>
                 <h3>{t('contacts-page.team-hq')}</h3>
-                <div className="first-line">Faculdade de Ciências e Tecnologia</div>
-                <div className="second-line">2829-516 Caparica</div>
+                <p className="first-line">Faculdade de Ciências e Tecnologia</p>
+                <p className="second-line">2829-516 Caparica</p>
               </div>
-              <div>
-                <h3 >{t('contacts-page.team-sm')}</h3>
+              <div className='contact-sub-section '>
+
                 <div className="contact-info-columns">
-                  <div>
-                    <h4 >{"Instagram"}</h4>
+                  <div className='contact-sub-section '>
+                    <h3 >{"Instagram"}</h3>
                     <div className='sm'><a href="https://www.instagram.com/novaformulastudent?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">@novaformulastudent</a></div>
                   </div>
-                  <div>
-                    <h4  >{t("LinkedIn")}</h4>
+                  <div className='contact-sub-section '>
+                    <h3  >{t("LinkedIn")}</h3>
                     <div className='sm'><a href="https://www.linkedin.com/company/formula-student-fct/" target="_blank" rel="noopener noreferrer">Nova Formula Student</a></div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          <div className='contact-form-section'>
+            <h2 className="form-title">{t('contacts-page.form-title')}</h2>
+            {feedbackMessage && (<div style={{ color: feedbackMessageType === 'success' ? 'green' : 'red' }}>
+              {feedbackMessage}
+            </div>)}
+            <form className='contact-form' ref={form} onSubmit={sendEmail}>
+              <div className='input-container'>
+                <input className='input' type='text' placeholder={t('contacts-page.form-subject')} />
+                <div class="cut"></div>
+
+              </div>
+              <div className='input-container'><input className='input' type='email' onChange={(e) => setSubject(e.target.value)} placeholder={t('contacts-page.form-email')} />
+              </div>
+              <div className='input-container-msg'><textarea className='input' onChange={(e) => setEmail(e.target.value)} placeholder={t('contacts-page.form-message')}></textarea>
+              </div>
+              <button className='submit' type='submit'>{t('contacts-page.form-send')} </button>
+            </form>
+          </div>
         </div>
       </div>
 
-      <MapComponent mapCenter={[38.66214021803797, -9.2070192353163]} mapZoom={16}  />
-      </div>
+      <MapComponent mapCenter={[38.66214021803797, -9.2070192353163]} mapZoom={16} />
+    </div>
   );
 }
 
